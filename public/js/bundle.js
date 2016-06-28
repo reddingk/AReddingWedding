6dce99d016a218fb39f255b72a564aa00e3f49ed
Kris Redding <kris.redding3@gmail.com>
2016-06-28 13:44:34 -0400

    Add background image

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -11,57 +11,6 @@
 })();
 
 (function(){
-  'use strict';
-
-  angular.module('config', [ 'ngMaterial' ]);
-
-})();
-
-(function(){
-
-  angular
-    .module('config')
-    .config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-      $stateProvider
-      .state('app', {
-        url: "/",
-        views: {
-          'content':{
-            templateUrl: 'views/home.html'
-            /*,controller: 'HomeController as hc'*/
-          },
-          'header':{
-            templateUrl: 'views/templates/_header.html',
-            controller: 'HeaderController as hdc'
-          }
-        }
-      })
-      .state('app.construction', {
-        url: "underconstruction",
-        views: {
-          'content@': {
-            templateUrl: 'views/construction.html'
-          }
-        }
-      })
-      .state('app.ourstory', {
-        url: "ourstory",
-        views: {
-          'content@': {
-            templateUrl: 'views/ourstory.html'/*,
-            controller: 'OurStoryController as oc'*/
-          }
-        }
-      });
-
-      $urlRouterProvider.otherwise('/');
-      //$locationProvider.html5Mode(true);
-    }]);
-
-
-})();
-
-(function(){
  "use strict";
 
   angular.module('headerCtrl').controller('HeaderController', ['$state', function($state){
@@ -141,5 +90,56 @@
     }
 
   }]);
+
+})();
+
+(function(){
+  'use strict';
+
+  angular.module('config', [ 'ngMaterial' ]);
+
+})();
+
+(function(){
+
+  angular
+    .module('config')
+    .config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+      $stateProvider
+      .state('app', {
+        url: "/",
+        views: {
+          'content':{
+            templateUrl: 'views/home.html'
+            /*,controller: 'HomeController as hc'*/
+          },
+          'header':{
+            templateUrl: 'views/templates/_header.html',
+            controller: 'HeaderController as hdc'
+          }
+        }
+      })
+      .state('app.construction', {
+        url: "underconstruction",
+        views: {
+          'content@': {
+            templateUrl: 'views/construction.html'
+          }
+        }
+      })
+      .state('app.ourstory', {
+        url: "ourstory",
+        views: {
+          'content@': {
+            templateUrl: 'views/ourstory.html'/*,
+            controller: 'OurStoryController as oc'*/
+          }
+        }
+      });
+
+      $urlRouterProvider.otherwise('/');
+      //$locationProvider.html5Mode(true);
+    }]);
+
 
 })();
