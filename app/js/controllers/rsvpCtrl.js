38a1166b62b2e73583005773a1d9406df35b2f6e
Kris Redding <kredding@autoscribe.com>
2016-12-21 14:44:22 -0500

    tweak styles

--- app/js/controllers/rsvpCtrl.js
+++ app/js/controllers/rsvpCtrl.js
@@ -5,17 +5,26 @@
     var vm = this;
     /*Functions*/
     vm.CheckKey = CheckKey;
+    vm.clearMsg = clearMsg;
     /*Variables*/
     vm.userkey = "";
     vm.rsvpKey = "ReddingWedding2018";
     vm.status = "";
+    vm.errorMsg = false;
 
     /**/
     function CheckKey() {
+      vm.errorMsg = false;
       if(vm.userkey.toLowerCase() == vm.rsvpKey.toLowerCase())
-      { vm.status = "UNLOCKED";}
+      { vm.status = "UNLOCKED";   }
       else
-      { vm.status = "LOCKED"; }
+      {
+        vm.errorMsg = true;
+        vm.status = "LOCKED";
+      }
+    }
+    function clearMsg(){
+      vm.errorMsg = false;
     }
 
   }]);
