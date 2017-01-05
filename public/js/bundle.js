03b3f8d3c8641fd7cf81ed363188efbbbfb4bfe9
Kris Redding <kredding@autoscribe.com>
2017-01-05 13:16:16 -0500

    add registry lists

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -985,11 +985,10 @@
     /*Variables*/
     vm.selected = null;
 
-    vm.items = [];
-    vm.items_demo = [
-      {"id":0, "name":"AMAZON", "url":"https://www.amazon.com/", "img":"amazon-logo.png" },
-      {"id":1, "name":"NEWLYWISH", "url":"https://www.newlywish.com/", "img":"newlywish.png"},
-      {"id":2, "name":"BED, BATH & BEYOND", "url":"http://www.bedbathandbeyond.com/", "img":"BedBathBeyond-Logo.png"}
+    //vm.items = [];
+    vm.items = [
+      {"id":0, "name":"TARGET", "url":"http://www.target.com/gift-registry/registry/939b9593b27d4ed8926a524b0ff0916d?ref=registryCopiedLink", "img":"Target-logo.png" },
+      {"id":1, "name":"BED, BATH & BEYOND", "url":"https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=544091480&eventType=Wedding&pwsurl=", "img":"BedBathBeyond-Logo.png"}
     ];
 
   }]);
