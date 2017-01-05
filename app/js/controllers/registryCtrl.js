59830d59ef392c1c631a6f634ecb506d967e031e
Kris Redding <kredding@autoscribe.com>
2017-01-05 14:50:21 -0500

    update landing screen

--- app/js/controllers/registryCtrl.js
+++ app/js/controllers/registryCtrl.js
@@ -10,8 +10,8 @@
 
     //vm.items = [];
     vm.items = [
-      {"id":0, "name":"TARGET", "url":"http://www.target.com/gift-registry/registry/939b9593b27d4ed8926a524b0ff0916d?ref=registryCopiedLink", "img":"Target-logo.png" },
-      {"id":1, "name":"BED, BATH & BEYOND", "url":"https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=544091480&eventType=Wedding&pwsurl=", "img":"BedBathBeyond-Logo.png"}
+      {"id":0, "name":"BED, BATH & BEYOND", "url":"https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=544091480&eventType=Wedding&pwsurl=", "img":"BedBathBeyond-Logo.png"},
+      {"id":1, "name":"TARGET", "url":"http://www.target.com/gift-registry/registry/939b9593b27d4ed8926a524b0ff0916d?ref=registryCopiedLink", "img":"Target-logo.png" }
     ];
 
   }]);
