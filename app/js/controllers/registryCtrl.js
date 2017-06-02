1c424349beea579c34cec89be7c19957bc371e97
Kris Redding <kredding@autoscribe.com>
2017-06-02 17:09:40 -0400

    update color and registry item

--- app/js/controllers/registryCtrl.js
+++ app/js/controllers/registryCtrl.js
@@ -10,9 +10,9 @@
 
     //vm.items = [];
     vm.items = [
-      {"id":0, "name":"BED, BATH & BEYOND", "url":"https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=544091480&eventType=Wedding&pwsurl=", "img":"BedBathBeyond-Logo.png"},
-      {"id":1, "name":"TARGET", "url":"http://www.target.com/gift-registry/registry/5e783267ee05492d85e307054cae10dc?ref=registryCopiedLink", "img":"Target-logo.png" },
-      {"id":2, "name":"WAYFAIR", "url":"https://www.wayfair.com/registry/wedding/5e5c3220-a323-4165-8aaa-e2c7493eaec1", "img":"Wayfair-logo.png" }
+      {"id":0, "name":"ZOLA", "url":"https://www.zola.com/registry/graceandkristopher", "img":"zola-logo-blue.png" },
+      {"id":1, "name":"TARGET", "url":"http://www.target.com/gift-registry/registry/5e783267ee05492d85e307054cae10dc?ref=registryCopiedLink", "img":"Target-logo.png" },      
+      {"id":2, "name":"BED, BATH & BEYOND", "url":"https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=544091480&eventType=Wedding&pwsurl=", "img":"BedBathBeyond-Logo.png"}
     ];
 
   }]);
